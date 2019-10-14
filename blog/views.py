3d0a5226956cd719203252d1e235d935b7a1788c
Debbie Thompson <deb246@btinternet.com>
2019-10-14 10:23:27 +0800

    blog app added to settings and blog view added in blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+
+
+def blog(request):
+    """A View that renders the cart contents page"""
+    return render(request, "blog.html")
