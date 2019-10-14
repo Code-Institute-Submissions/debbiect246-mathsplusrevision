f5e93bbc3a15f6dd66e59498a61deaa80e9c21fe
Debbie Thompson <deb246@btinternet.com>
2019-10-14 15:26:01 +0800

    urls files amended

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,1 @@
 from django.conf.urls import url, include
-from .import urls_reset
-from .views import blog
-
-
-urlpatterns = [
-    url(r'^blog/$', blog, name='blog'),
-]
--- mathsplusrevision/urls.py
+++ mathsplusrevision/urls.py
@@ -35,6 +35,9 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
-    url(r'^blog/', include(urls_blog)),
+    url(r'^$', RedirectView.as_view(url='blog/')),
+    url(r'^blog/', include('blog.urls')),
+
+
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
