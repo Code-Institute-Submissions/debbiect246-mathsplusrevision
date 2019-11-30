896253412560aea9efb83aee2910363c20082fc3
Debbie Thompson <deb246@btinternet.com>
2019-11-30 19:24:43 +0000

    added in code for home page

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^profile/$', profile, name='profile'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
+    url(r'^index/$', index, name='home'),
     url(r'^password-reset/', include(urls_reset)),
 
 
