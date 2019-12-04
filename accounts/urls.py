ce00a84e88068a7cb417ded28988952b5eb95353
Debbie Thompson <deb246@btinternet.com>
2019-12-04 00:01:48 +0000

    got index.html page to render as home page

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^profile/$', profile, name='profile'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
-    url(r'^index/$', index, name='home'),
+    url(r'^index/$', index, name='index'),
     url(r'^password-reset/', include(urls_reset)),
 
 ]
