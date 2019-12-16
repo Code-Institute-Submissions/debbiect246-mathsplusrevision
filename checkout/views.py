600fb4e0dff6588bdb2d0fe8cf6f3444d1f86da3
Debbie Thompson <deb246@btinternet.com>
2019-12-16 21:07:35 +0000

    changed payment form year choices to 2020 to 2036

--- checkout/views.py
+++ checkout/views.py
@@ -17,12 +17,10 @@ def checkout(request):
     if request.method == "POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
-        user = request.user
 
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
-            order.userprofile = user
             order.save()
 
             cart = request.session.get('cart', {})
