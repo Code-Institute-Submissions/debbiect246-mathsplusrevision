4fad259d01f9c9ca6db88ad29ed82776d4432e02
Debbie Thompson <deb246@btinternet.com>
2019-12-22 20:59:07 +0000

    sorted out literal error in cart so user now must choose a quantity

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,14 +11,20 @@ def view_cart(request):
 
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
+    try:
+        quantity = int(request.POST.get('quantity'))
 
-    quantity = int(request.POST.get('quantity'))
+        cart = request.session.get('cart', {})
+        cart[id] = cart.get(id, quantity)
 
-    cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+        request.session['cart'] = cart
+        return redirect(reverse('view_cart'))
 
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+    except ValueError:
+        messages.error(request, "You must chose a quantity for this paper, ")
+        messages.error(
+            request, "select the papers tab and choose a quanity to order.")
+        return render(request, "cart.html")
 
 
 def adjust_cart(request, id):
