0cd69c21840fc3682dd8a519597f6789128dc6f3
Debbie Thompson <deb246@btinternet.com>
2019-12-30 22:07:45 +0000

    tried to test posts model

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class PostTests(TestCase):
+    """
+    Here we'll define the tests that we'll run against our
+    Product model
+    """
+
+    def test_str(self):
+        test_name = title(name='Blog title')
+        self.assertEqual(str(test_name), 'Blog title')
