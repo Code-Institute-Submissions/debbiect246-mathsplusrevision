904434757d58a57fb00c27742e1e76536ec73575
Debbie Thompson <deb246@btinternet.com>
2020-01-23 11:44:04 +0000

    comments added to urls.py in top level app

--- mathsplusrevision/urls.py
+++ mathsplusrevision/urls.py
@@ -13,21 +13,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# imported libraries for project
+# url needed to handle urls
 from django.conf.urls import url, include
+# redirectview needed for redirecting users to another page when necessary
 from django.views.generic import RedirectView
+# serve needed to serve static files
 from django.views.static import serve
+# admin needed for the django dashboard
 from django.contrib import admin
+# path needed to direct data along correct path
 from django.urls import path
+# accounts needed to import account urls
 from accounts import urls as urls_accounts
+# urls for products imported
 from products import urls as urls_products
+# urls for posts imported
 from posts import urls as urls_posts
+# urls for cart imported
 from cart import urls as urls_cart
+# urls from search app imported
 from search import urls as urls_search
+# urls from checkout app imported
 from checkout import urls as urls_checkout
+# urls from product app imported
 from products.views import all_products
+# urls from all views to host static files imported
 from django.views import static
+# media root needed to handle all media files
 from .settings import MEDIA_ROOT
 
+# url patterns for all pages on website
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
