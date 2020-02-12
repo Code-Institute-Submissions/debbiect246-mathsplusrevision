3c5cab319a3e58501c6a69975229daac0254eaf0
Debbie Thompson <deb246@btinternet.com>
2020-02-12 14:29:14 +0800

    added comments explaining why user profile is not fully functioning yet

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
+'''
+user profile handler, imports user profile model and 
+works with admin to set up a profile for user.
+Not fully functional as unable to connect user orders to
+profile, despite spending 3 hours talking to tutors and 
+some help from slack room.  Future area for development.
+'''
+
 from django.contrib import admin
 
 from .models import UserProfile
