a454cd71779ab805fe55d5a19dc31b6b80eec946
Debbie Thompson <deb246@btinternet.com>
2020-02-18 11:20:44 +0800

    comments added for post app

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,11 @@
+'''
+libraries imported for posts app.
+'''
 from django.conf.urls import url, include
 from django.conf.urls import url
 from .views import get_posts, post_detail, posts
 
+# urls patterns for posts app
 urlpatterns = [
     url(r'^$', get_posts, name='get_posts'),
     url(r'^$', posts, name='posts'),
