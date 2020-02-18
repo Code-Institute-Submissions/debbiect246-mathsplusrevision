15a685253e14ffbe7d6e2ea221894ac75dd0ba0d
Debbie Thompson <deb246@btinternet.com>
2020-02-18 11:25:40 +0800

    explanatory comments added for search app

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,13 @@
+'''
+libraries added for search app.
+'''
 from django.conf.urls import url
 from .views import do_search
 
-
+'''
+url pattern for search app.  This app allows a user to search
+for an item in the products model.
+'''
 urlpatterns = [
     url(r'^$', do_search, name='search')
 
--- search/views.py
+++ search/views.py
@@ -1,11 +1,20 @@
+'''
+render library imported from django
+Product model imported into search app as this will 
+be searched for a product.
+'''
 from django.shortcuts import render
 from products.models import Product
 
 
 # Create your views here.
+'''
+this function does the search so that when user presses
+search button item is found from products model, or nothing is returned if item
+is not in product model.
+'''
 
 
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
-# this may not run with sql database
